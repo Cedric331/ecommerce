a4e10eb105c457e3627c250951fe4f97deebc600
Cedric Lima <limacedric@hotmail.fr>
2021-01-17 11:20:51 +0100

    Ajout des transporteurs dans la section admin

---
+++ src/Controller/Admin/CarrierCrudController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Carrier;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class CarrierCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Carrier::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+     return $crud
+         // the labels used to refer to this entity in titles, buttons, etc.
+         ->setPaginatorPageSize(15)
+         ->setEntityLabelInSingular('Transporteur')
+         ->setEntityLabelInPlural('Transporteurs');
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name', 'Nom'),
+            TextEditorField::new('description'),
+            MoneyField::new('price', 'Prix')->setCurrency('EUR'),
+         ];
+    }
+    
+}
